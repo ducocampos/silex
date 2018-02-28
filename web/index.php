7b735c62ea560e5498b00c842a4671fd1eb9fe81
Eduardo <eduardo@debian>
2018-02-27 21:58:09 -0300

    Terminado o projeto 1 do curso de Silex.

--- web/index.php
+++ web/index.php
@@ -2,15 +2,32 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
-$data = '24/02/2018';
+$posts = array(
+	1 => 'Esse é o post número 1.', 
+	2 => 'Esse é o post número 2.', 
+	3 => 'Esse é o post número 3.', 
+	4 => 'Esse é o post número 4.', 
+	5 => 'Esse é o post número 5.',
+	6 => 'Esse é o post número 6.',
+	7 => 'Esse é o post número 7.',
+	8 => 'Esse é o post número 8.',
+	9 => 'Esse é o post número 9.',
+	10 => 'Esse é o post número 10.',
+);
 
-$app->get('/hello', function() use($data)
-{
-	return 'Olá mundo! <br>Hoje é: ' . $data;
+$app->get('/posts/{id}', function($id) use($posts) {
+	
+	if(!isset($posts[$id]))
+		return new Response('Post não encontrado. Favor verificar se o número do mesmo está correto.',404);
+	
+	return new Response('Post id: ' . $id . '<br>' . 'Conteúdo: ' . $posts[$id], 200);
+
 });
 
 $app->run();
