5f0410d842084d224f2dd62dcae72182aa657774
Eduardo <eccampos@gmail.com>
2018-03-03 19:51:13 -0300

    Movido o array  para o arquivo posts.php

--- web/posts.php
+++ web/posts.php
@@ -4,6 +4,19 @@ use \Symfony\Component\HttpFoundation\Response;
 
 $posts = $app['controllers_factory'];
 
+$posts1 = array(
+	1 => 'Esse é o post número 1.', 
+	2 => 'Esse é o post número 2.', 
+	3 => 'Esse é o post número 3.', 
+	4 => 'Esse é o post número 4.', 
+	5 => 'Esse é o post número 5.',
+	6 => 'Esse é o post número 6.',
+	7 => 'Esse é o post número 7.',
+	8 => 'Esse é o post número 8.',
+	9 => 'Esse é o post número 9.',
+	10 => 'Esse é o post número 10.',
+);
+
 $posts->get("/", function() use($posts1) {
 
 	foreach ($posts1 as $key => $value) {
